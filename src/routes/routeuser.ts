af9c431412a454d398499cbc69aea1016d92e1ff
Miruin <44851774+Miruin@users.noreply.github.com>
2021-11-14 19:36:36 -0400

    Update routeuser.ts

--- src/routes/routeuser.ts
+++ src/routes/routeuser.ts
@@ -17,6 +17,21 @@ class Rutasuser{
 
     routes() {
         
+        this.router.options('/registro', (req: Request, res: Response, next: any) =>{
+
+            res.setHeader('Access-Control-Allow-Origin', '*');
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+            res.setHeader('Access-Control-Allow-Credentials', 'true');
+            // handle OPTIONS method
+            if ('OPTIONS' == req.method) {
+                return res.sendStatus(200);
+            } else {
+                 next();
+            }
+
+        })
+        
         this.router.post('/registro', cu.reguser)
 
         this.router.post('/log', cu.login)
